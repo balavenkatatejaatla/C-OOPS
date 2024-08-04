5e91446a4a2490114a37b7b752ed50842275092f
balavenkatatejaatla <balavenkatatejaatla@gmail.com>
2024-08-04 19:02:36 -0400

    Create Encapsulation.cs

---
+++ Encapsulation.cs
@@ -0,0 +1,11 @@
+public class Student
+{
+    private string name;
+    private int age;
+
+    public string GetName() => name;
+    public void SetName(string name) => this.name = name;
+
+    public int GetAge() => age;
+    public void SetAge(int age) => this.age = age;
+}
